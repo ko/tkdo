38193c2d0c0a20910ee378da2999f6a13b740908
Ken Ko <git@yaksok.net>
2015-09-06 16:35:40 -0700

    add "n days ago / Today" helper on Date()s

---
+++ app/helpers/date-days-ago.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export function dateDaysAgo(params/*, hash*/) {
+    let date = new Date(params.get(0));
+    let today = new Date();
+    let dateAgoMs = Math.abs(today - date);
+    let dateAgoDays = Math.round(dateAgoMs / 3600 / 24 / 1000);
+
+    if (dateAgoDays == 0) {
+        return "Today";
+    } else {
+        return dateAgoDays + " days ago";
+    }
+}
+
+export default Ember.Helper.helper(dateDaysAgo);
---
+++ tests/unit/helpers/date-days-ago-test.js
@@ -0,0 +1,10 @@
+import { dateDaysAgo } from '../../../helpers/date-days-ago';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | date days ago');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = dateDaysAgo(42);
+  assert.ok(result);
+});
